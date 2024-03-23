d1738dbd18746a852bfd8043a3326f17826ce01c
JimGitFE <rymsjfjrys@gmail.com>
2024-03-23 02:33:16 -0300

    enable withdrawals

--- cryptoPass.sol
+++ cryptoPass.sol
@@ -7,6 +7,8 @@ import "./tools.sol";
 contract cryptoPass {
     // Variables 0xAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAF
     // Variables 0xAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAF
+
+    // Contract Variables
     using priceTools for int256;
     using addressTools for address[];
 
@@ -15,65 +17,129 @@ contract cryptoPass {
     AggregatorV3Interface public priceFeed;
     address[] public auhtorizedSpender;
 
+    // Scenario Variables
     struct Depositor {
-        address depositor;
+        // address depositor;
         uint deposited;
+        uint depositsCount;
     }
     
     struct Car {
         uint balance;
-        Depositor[] depositors;
-        
+        mapping(address => Depositor) depositor;
+
+        address[] depositors;
+        mapping(address => bool) depositorExist;
     }
+    mapping(bytes16 => Car) public realBalance;
+    // address => plates
+    mapping(address => bytes16[]) public addrToPlates;
     // plate => balance
-    mapping(bytes16 => uint) public balance;
-    mapping(bytes16 => Car) public balance2;
+    // mapping(bytes16 => uint) public balance;
 
     constructor() {
         priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         auhtorizedSpender.push(payable(msg.sender));
     }
 
+    function getBalance (bytes16 _plate) public view returns(uint bal) {
+      return realBalance[_plate].balance;
+    }
+    function getDepositors (bytes16 _plate) public view returns(address[] memory depositors) {
+      return realBalance[_plate].depositors;
+    }
+    function getDeposited (bytes16 _plate) public view returns(uint depositors) {
+      // address person = realBalance[_plate].depositors[0];
+      return realBalance[_plate].depositor[msg.sender].deposited;
+    }
+    // function getDepositorsExist (bytes16 _plate) public view returns(bool exists) {
+    //   return realBalance[_plate].depositorExist[msg.sender];
+    // }
+    function getDepositorCount (bytes16 _plate) public view returns(uint counter) {
+      return realBalance[_plate].depositor[msg.sender].depositsCount;
+    }
+
+    // function addDeposit(bytes16 _plate, uint _deposited) private view returns(address asd) {
+    function addDeposit(bytes16 _plate) public payable returns(uint count) {
+      require(msg.value>0, "No charge");
+      Car storage car = realBalance[_plate];
+      Depositor storage account = car.depositor[msg.sender];
+
+      // Add deposited value 
+      car.balance += msg.value;
+      account.deposited += msg.value;
+      account.depositsCount += 1;
+      // Add address => plates
+      addrToPlates[msg.sender].push(_plate);
+
+      // Adding to array of depositors
+      if (!car.depositorExist[msg.sender]) {
+        car.depositors.push(msg.sender);
+        car.depositorExist[msg.sender] = true;
+      }
+      // needed?
+      // Common Practice, mapping with array
+
+
+      return account.depositsCount;
+    }
     // Functions
-    function spend(bytes16 _plate, uint _value) public onlyAuhtorized returns (Depositor[] memory) {
-        balance[_plate] -= _value;
+    function spend(bytes16 _plate, uint _value) public onlyAuhtorized returns (uint tobespentleft) {
+      Car storage car = realBalance[_plate];
 
-        Depositor[] memory depositorsCopy = balance2[_plate].depositors;
-        // for (uint i = 0; i < 2 - 1; i++) {
-        //     // if exceeds value added by depositor
-        //     depositorsCopy = depositorsCopy.removeFirstDepositor();
-        // }
+      uint toBeSpent = _value;
 
-        // payable(msg.sender).transfer(_value);
-        return depositorsCopy;
+      // Spend for each depositor until _value is reached
+      for (uint i = 0; i < car.depositors.length; i++) {
+        Depositor storage accountI = car.depositor[car.depositors[i]];
+        
+        // Subtract spent amount
+        if (accountI.deposited > toBeSpent) {
+          accountI.deposited -= toBeSpent;
+          car.balance -= toBeSpent;
+          toBeSpent -= toBeSpent;
+
+          // Remove User from Array
+        } else {
+          car.balance -= accountI.deposited;
+          toBeSpent -= accountI.deposited;
+          accountI.deposited -= accountI.deposited;
+
+          break;
+        }
+        // car.depositors[i];
+        // remove spent depositors
+      }
+      require(toBeSpent == 0, "Insufficient Balance");
+      return toBeSpent;
     }
 
-    function addDeposit(bytes16 _plate, uint _deposited) public returns(address asd) {
-        // Depositor[] memory _depositors = balance2[_plate].depositors;
+    function withdraw(uint _amount) public {
+      uint toBeWithdrawn = _amount;
+      bytes16[] storage toBeRemoved;
 
-        // balance2[_plate].balance = _deposited;
-        // _depositors[0].deposited = 3e10;
-        // _depositors[0].depositor = msg.sender;
-        // balance2[_plate].depositors = _depositors;
-        // return _depositors[0].depositor;
-    }
+      for(uint i = 0; i < addrToPlates[msg.sender].length; i++) {
+          Car storage car =  realBalance[addrToPlates[msg.sender][i]];
+          Depositor storage user = car.depositor[msg.sender];
 
-    function getCar (bytes16 _plate) public view returns(Car memory) {
-        return balance2[_plate];
-    }
+          // Subtract withdrawn amount
+          if(toBeWithdrawn >= user.deposited) { 
+            car.balance -= user.deposited;
+            toBeWithdrawn -= user.deposited;
+            user.deposited -= user.deposited;
+            
+            // Remove User from Array
+          } else {
+            user.deposited -= toBeWithdrawn;
+            car.balance -= toBeWithdrawn;
+            toBeWithdrawn -= toBeWithdrawn;
 
-    function addFunds(bytes16 _plate) public {
-        // require(int256(msg.value).convertETHtoUSD(priceFeed)>=minimumDeposit, "minimumDeposit");
-        // for (uint256 depositorIndex=0; depositorIndex < balance2[_plate].depositors.length; depositorIndex++) {
-
-        // }
-        addDeposit(_plate, 25);
-        balance[_plate] += 25;
-        // return arrayLength+1;
-        // uint arrayLength = balance2[_plate].depositors.length;
-        // balance2[_plate].balance += 25;
-        // balance2[_plate].depositors[arrayLength+1].push(msg.sender, 25e10);
-
+            break;
+          }
+      }
+      (bool sent, ) = msg.sender.call{value: _amount}("");
+      require(toBeWithdrawn==0, "Not enough deposited");
+      require(sent, "Failed to withdraw");
     }
 
     // Modifiers
@@ -83,10 +149,10 @@ contract cryptoPass {
     }
 
     receive() external payable {
-      addDeposit(0xAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAF, msg.value);
+      addDeposit(0xAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAF);
     }
     fallback(bytes calldata input) external payable returns (bytes memory) {
-      addDeposit(0xAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAF, msg.value);
+      addDeposit(0xAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAF);
       return input;
     }
 }
